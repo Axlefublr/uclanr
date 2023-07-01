6adb47dbc0e6cabce86acaa80ee46907bef5c29e
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-07-02 04:54:40 +0800

    add read

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
+use serde_json;
+use serde_json::Value as JsonValue;
 use std::env;
+use std::error::Error;
+use std::fs::File;
+use std::io::BufReader;
 use std::path::Path;
 use std::path::PathBuf;
 
 fn main() {
+	let file = get_json_path();
+	let contents = read(file).unwrap();
+	println!("{:#?}", contents)
+}
 
 fn get_json_path() -> PathBuf {
 	env::current_exe()
@@ -17,4 +26,9 @@ fn get_json_path() -> PathBuf {
 		.join("words.json")
 }
 
+fn read<P: AsRef<Path>>(path: P) -> Result<JsonValue, Box<dyn Error>> {
+	let file = File::open(path)?;
+	let reader = BufReader::new(file);
+	let json = serde_json::from_reader(reader)?;
+	Ok(json)
 }
