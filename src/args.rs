90fd513d930a8c3af790680183c5af73c376084b
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-07-20 19:22:53 +0800

    add title & caps casing optional functionality
    pair programmed with @TheSast

--- src/args.rs
+++ src/args.rs
@@ -1,9 +1,26 @@
 ﻿use clap::Parser;
+use crate::Case;
 
 #[derive(Parser, Debug)]
 #[command(author, about, long_about = None)]
 pub struct Args {
 	#[arg(short, long, default_value_t = String::from(" "))]
 	pub joiner: String,
+	#[arg(short, long)]
+	pub caps: bool,
+	#[arg(short, long)]
+	pub title: bool,
 	pub amount: Option<usize>,
+}
+
+impl Args {
+	pub fn get_case(&self) -> Case {
+		if self.caps {
+			Case::Caps
+		} else if self.title {
+			Case::Title
+		} else {
+			Case::Lower
+		}
+	}
 }
--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,9 @@ fn main() {
 		.lines()
 		.collect::<Result<_, _>>()
 		.expect("file parsed correctly");
-	let words = get_random_words(&words, args.amount.unwrap_or(1));
-	println!("{}", words.join(&args.joiner));
+	let mut picked_words = get_random_words(&words, args.amount.unwrap_or(1));
+	alter_case(&mut picked_words, args.get_case());
+	println!("{}", picked_words.join(&args.joiner));
 }
 
 fn get_random_words(words: &[String], amount: usize) -> Vec<String> {
@@ -20,4 +21,30 @@ fn get_random_words(words: &[String], amount: usize) -> Vec<String> {
 		.choose_multiple(&mut rand::thread_rng(), amount)
 		.cloned()
 		.collect()
+}
+
+pub enum Case {
+	Caps,
+	Title,
+	Lower
+}
+
+fn alter_case(words: &mut [String], case: Case) {
+	match case {
+		Case::Caps => {
+			words
+				.iter_mut()
+				.for_each(|x| {
+					x.make_ascii_uppercase();
+				});
+		}
+		Case::Title => {
+			words
+				.iter_mut()
+				.for_each(|x| {
+					x[0..1].make_ascii_uppercase();
+				});
+		}
+		Case::Lower => ()
+	}
 }
