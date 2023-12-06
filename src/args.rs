98c065fd4d36193bb68f1e557c9172d3e4b7492d
Axlefublr <101342105+Axlefublr@users.noreply.github.com>
2023-12-06 19:09:12 +0800

    formatting

--- src/args.rs
+++ src/args.rs
@@ -3,64 +3,61 @@ use clap::{Parser, ValueEnum};
 #[derive(Parser)]
 #[command(author, about, next_line_help = true)]
 pub struct Args {
-	/// The string that joines the random words, if there are more than 1
-	#[arg(short, long, default_value_t = String::from(" "))]
-	joiner: String,
+    /// The string that joines the random words, if there are more than 1
+    #[arg(short, long, default_value_t = String::from(" "))]
+    joiner: String,
 
-	/// Sets the case of every word
-	#[arg(short, long, value_enum, default_value_t = Case::Lower)]
-	case: Case,
+    /// Sets the case of every word
+    #[arg(short, long, value_enum, default_value_t = Case::Lower)]
+    case: Case,
 
-	/// Disable interpreting \n as a newline and \t as a tab
-	#[arg(short, long)]
-	raw: bool,
+    /// Disable interpreting \n as a newline and \t as a tab
+    #[arg(short, long)]
+    raw: bool,
 
-	/// Amount of random words to print
-	#[arg(default_value_t = 1)]
-	pub amount: usize,
+    /// Amount of random words to print
+    #[arg(default_value_t = 1)]
+    pub amount: usize,
 }
 
 impl Args {
-	pub fn get_joiner(&self) -> String {
-		if self.raw {
-			self.joiner.clone()
-		} else {
-			self.joiner
-				.clone()
-				.replace("\\n", "\n")
-				.replace("\\t", "\t")
-		}
-	}
+    pub fn get_joiner(&self) -> String {
+        if self.raw {
+            self.joiner.clone()
+        } else {
+            self.joiner.clone().replace("\\n", "\n").replace("\\t", "\t")
+        }
+    }
 
-	pub fn alter_case(&self, mut words: Vec<String>) -> Vec<String> {
-		self.case.alter_case(&mut words);
-		words.to_vec()
-	}
+    pub fn alter_case(&self, mut words: Vec<String>) -> Vec<String> {
+        self.case.alter_case(&mut words);
+        words.to_vec()
+    }
 }
 
 #[derive(ValueEnum, Clone, Copy)]
 pub enum Case {
-	Caps,
-	Title,
-	Lower,
+    Caps,
+    Title,
+    Lower,
 }
 
 impl Case {
-	fn alter_case(self, words: &mut [String]) {
-		match self {
-			Self::Caps => {
-				for x in words.iter_mut() {
-					x.make_ascii_uppercase();
-				}
-			}
+    fn alter_case(self, words: &mut [String]) {
+        match self {
+            Self::Caps => {
+                for x in words.iter_mut() {
+                    x.make_ascii_uppercase();
+                }
+            }
 
-			Self::Title => {
-				for x in words.iter_mut() {
-					x[0..1].make_ascii_uppercase();
-				}
-			}
+            Self::Title => {
+                for x in words.iter_mut() {
+                    x[0..1].make_ascii_uppercase();
+                }
+            }
 
-			Self::Lower => {}
-		}
-	}
+            Self::Lower => {}
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,15 @@ mod args;
 mod words;
 
 fn main() {
-	let args = args::Args::parse();
-	let picked_words = args.alter_case(get_random_words(args.amount));
+    let args = args::Args::parse();
+    let picked_words = args.alter_case(get_random_words(args.amount));
 
-	println!("{}", picked_words.join(&args.get_joiner()));
+    println!("{}", picked_words.join(&args.get_joiner()));
 }
 
 fn get_random_words(amount: usize) -> Vec<String> {
-	WORDS
-		.choose_multiple(&mut rand::thread_rng(), amount)
-		.map(|word| (*word).to_string())
-		.collect()
+    WORDS
+        .choose_multiple(&mut rand::thread_rng(), amount)
+        .map(|word| (*word).to_string())
+        .collect()
 }
